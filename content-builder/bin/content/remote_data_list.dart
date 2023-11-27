5c9e0204a2b3cd7d59beaeefe4db0905a223578c
abdulsamad <samadfullstack@gmail.com>
2023-11-27 20:13:57 +0500

    changes

--- content-builder/bin/content/remote_data_list.dart
+++ content-builder/bin/content/remote_data_list.dart
@@ -4,6 +4,7 @@ import '../models/tech_ids.dart';
 RemoteDataList remoteDataList = RemoteDataList(list: [
   reactjsRemoteData,
   tailwindCssData,
+  testData,
 ]);
 
 var reactjsRemoteData = RemoteDataModel(
@@ -19,5 +20,13 @@ var tailwindCssData = RemoteDataModel(
   version: 0,
   objUrl:
       "https://raw.githubusercontent.com/samadfullstack/doks-content/master/data/tailwindcss.json",
+  pendingDownload: true,
+);
+
+var testData = RemoteDataModel(
+  id: TechIds.test,
+  version: 0,
+  objUrl:
+      "https://raw.githubusercontent.com/samadfullstack/doks-content/master/data/test.json",
   pendingDownload: true,
 );
