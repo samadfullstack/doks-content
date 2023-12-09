d06046652fa1b59639bbbf8269ed9e80a33720bf
abdulsamad <samadfullstack@gmail.com>
2023-12-09 14:32:53 +0500

    changes

--- content-builder/bin/content/test/docs.dart
+++ content-builder/bin/content/test/docs.dart
@@ -7,31 +7,21 @@ List<DocModel> testDocs = [
   ...Images.values.map((item) => DocModel(
       docTitle: item.name,
       id: item.prefix,
-      url: MkLink.content(TechIds.reactJs.name, item.name)
-      // url: "$reactJsBaseUrl/content/${item.name}.md",
-      )),
+      url: MkLink.content(TechIds.test.name, item.name))),
   ...Hooks.values.map((item) => DocModel(
       docTitle: item.name,
       id: item.prefix,
-      url: MkLink.content(TechIds.reactJs.name, item.name)
-      // url: "$reactJsBaseUrl/content/${item.name}.md",
-      )),
+      url: MkLink.content(TechIds.reactJs.name, item.name))),
   ...Components.values.map((item) => DocModel(
       docTitle: item.name,
       id: item.prefix,
-      url: MkLink.content(TechIds.reactJs.name, item.name)
-      // url: "$reactJsBaseUrl/content/${item.name}.md",
-      )),
+      url: MkLink.content(TechIds.reactJs.name, item.name))),
   ...Apis.values.map((item) => DocModel(
       docTitle: item.name,
       id: item.prefix,
-      url: MkLink.content(TechIds.reactJs.name, item.name)
-      // url: "$reactJsBaseUrl/content/${item.name}.md",
-      )),
+      url: MkLink.content(TechIds.reactJs.name, item.name))),
   ...Directives.values.map((item) => DocModel(
       docTitle: item.name,
       id: item.prefix,
-      url: MkLink.content(TechIds.reactJs.name, item.name)
-      // url: "$reactJsBaseUrl/content/${item.name}.md",
-      )),
+      url: MkLink.content(TechIds.reactJs.name, item.name))),
 ];
