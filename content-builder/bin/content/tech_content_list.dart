875aca6a4e4eb0e2fb5cc9caa1dd2ab2fc894ec6
abdulsamad <samadfullstack@gmail.com>
2023-12-18 02:29:32 +0500

    changes

--- content-builder/bin/content/tech_content_list.dart
+++
@@ -1,6 +0,0 @@
-import '../abstract_classes/abstract_tech_data.dart';
-import 'data/reactjs.dart';
-
-List<AbstractTechData> techContentList = [
-  Reactjs(),
-];
--- content-builder/bin/content_builder.dart
+++
@@ -1,6 +0,0 @@
-import 'utils/content_writer.dart';
-
-void main(List<String> args) async {
-  await writeDataAndDocs();
-  await writeMetaData();
-}
